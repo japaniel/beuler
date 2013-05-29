0885336196bc3ef21f0e4efae0597594852799c6
Daniel Redington <japaniel@japaniel.com>
2013-05-29 21:31:53 +0000

    Sum all the multples

--- 1.js
+++ 1.js
@@ -1,13 +1,12 @@
 // http://projecteuler.net/problem=1
 ////////////////////////////////////
 
-var results = [];
+var result =0;
 
 for (var i = 1; i < 1000; i++) {
 	if (i % 3 === 0 || i % 5 === 0) {
-		results.push(i)
+		result += i;
 	}
 }
 
-result = results.join(', ');
 console.log(result);
