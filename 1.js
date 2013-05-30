55f5fb7cf6099ec2de1f8205f2d7d1c1f1a24cf3
Daniel Redington <japaniel@japaniel.com>
2013-05-30 01:02:15 +0000

    Made Number.isMethodof()

--- 1.js
+++ 1.js
@@ -1,10 +1,17 @@
 // http://projecteuler.net/problem=1
 ////////////////////////////////////
 
-var result =0;
+var result = 0;
+
+Number.prototype.isMultipleof = function (num) {
+	if (this % num === 0) {
+		return true;
+	}
+	return false;
+}
 
 for (var i = 1; i < 1000; i++) {
-	if (i % 3 === 0 || i % 5 === 0) {
+	if (i.isMultipleof(3) || i.isMultipleof(5) ) {
 		result += i;
 	}
 }
